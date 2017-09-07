a0e2306397bca07c6fae8bc0039534130aa7d6b1
unknown <mirandas.angelica@gmail.com>
2017-09-07 17:53:56 -0500

    Change bg color

--- Quote/src/app/quote/quote.component.ts
+++ Quote/src/app/quote/quote.component.ts
@@ -20,11 +20,21 @@ export class QuoteComponent implements OnInit {
   headerObj = {headers : new Headers(this.headersCont),
   };
 
+  getColor(){
+    var colors=["#E3D900", "#28CF98","#63E427","#F75C19",
+    "#42CFE5", "#C880EA", "#FC30F0", "#FC30A9","#FC306B","#FC303F",
+    "#FF6537","#FE961F","#A4F225","#40B03B","#3BB099","#B03B66"];
+    var index = Math.floor(Math.random()*colors.length);
+    return colors[index]
+  }
+
   getQ(){
+    document.body.style.backgroundColor = this.getColor();    
     this.http.get("https://andruxnet-random-famous-quotes.p.mashape.com/",this.headerObj).subscribe((res: Response) => this.quote = res.json());
   }
 
   ngOnInit() {
+    this.getColor();
     this.getQ();
   }
 
