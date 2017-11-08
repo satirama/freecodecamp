1fc455ac103c6c1320869d6ccb232d7157bcb009
unknown <mirandas.angelica@gmail.com>
2017-11-07 21:31:35 -0600

    buttons ready, timer: first try

--- pomodoro/README.md
+++ pomodoro/README.md
@@ -1,3 +1,8 @@
+---------------------------------------------------------------------------------
+
+Try it at : 
+
+----------------------------------------------------------------------------------
 # Pomodoro
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.3.0.
--- pomodoro/src/app/app.component.ts
+++ pomodoro/src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  time = 0;
+  play = 5;
+  work = 25;
+  mins = 0;
+  secs = '00';
+
+  status = 'work';
+
+  private sub: Subscription;  
+
+  ngOnInit(){
+    this.title= 'click to begin!';
+    this.mins= this.work;
+  }
+
+  counter(t){
+    this.time -= t;
+    while (this.time > 0){
+      if (Number(this.secs) === 0) {
+       this.secs = ('0' + (60 - t)).slice(-2);
+       this.mins -= 1;
+      }
+    }
+    this.sub.unsubscribe();
+    this.secs = '00';
+    if (this.status === 'work'){
+      this.status = 'play';
+      this.title = 'time to play!';
+    }
+    else{
+      this.status = 'play';
+      this.title = 'time to work!';
+    }
+  }
+
+  onOff(){
+    let timer = Observable.timer(0,1000);
+    this.sub = timer.subscribe((t) =>{
+      this.counter(t);
+    });
+
+    if (this.status === 'work'){
+      this.mins = this.work;
+      this.secs = '00';
+      this.time = this.work * 60
+    }
+    else{
+      this.mins = this.play;
+      this.secs = '00';
+      this.time = this.play * 60
+    }
+  }
+
+  playMore(){
+    this.play += 1;
+  }
+
+  playLess(){
+    this.play -= 1;
+  }
+
+  workMore(){
+    this.work += 1;
+  }
+
+  workLess(){
+    this.work -= 1;
+  }
 }
